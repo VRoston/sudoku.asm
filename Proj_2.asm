4890bc2aa072f4d740a957f0ac0bab840f011a44
Victor de Melo Roston <111363981+VRoston@users.noreply.github.com>
2022-12-08 14:33:03 -0300

    Update Proj_2.asm

--- Proj_2.asm
+++ Proj_2.asm
@@ -13,6 +13,12 @@ PULA_LINHA MACRO
     INT 21H
 ENDM
 
+ESPACO MACRO 
+    MOV AH, 02
+    MOV DL, 32
+    INT 21H    
+ENDM
+
 IMPRIME_MSG MACRO
     MOV AH, 09H         ; mostra na tela o que estivel em DL
     INT 21H
@@ -54,28 +60,69 @@ POPREGISTRADOR  MACRO
     POP AX 
 ENDM
 
+LIMPA_REGISTRADOR MACRO
+    XOR AX, AX
+    XOR BX, BX
+    XOR CX, CX
+    XOR DX, DX
+    XOR SI, SI
+ENDM
+
 .DATA
 ; mensagens
-    MSG1 DB 10, 13, '              PLAY SUDOKU', '$'
-    MSG2 DB 10, 13, '  ===[APERTE UMA TECLA PARA JOGAR]===', '$'
+    MSG1  DB 10, 13, '              PLAY SUDOKU', '$'
+    MSG2  DB 10, 13, '  ===[APERTE UMA TECLA PARA JOGAR]===', '$'
+    MSG3  DB 10, 13, '  AS REGRAS:', '$'
+    MSG4  DB 10, 13, '    1- O JOGO CONSISTE EM PREENCHER OS ESPACOS ONDE ESTA 0 COM OS NUMEROS DE 1 A 9', '$'
+    MSG5  DB 10, 13, '       CADA LINHA E COLUNA NAO DEVE CONTER OS NUMEROS DE 1 A 9 REPETIDOS', '$'
+    MSG6  DB 10, 13, '    2- O JOGO POSSUI 3 NIVEIS DE DIFICULDADE: FACIL, MEDIO, DIFICL', '$'
+    MSG7  DB 10, 13, '    3- PARA JOGAR DIGITE O NUMERO DA COLUNA E DA LINHA AO QUAL VOCE QUER ATRIBUIR O VALOR', '$'
+    MSG8  DB 10, 13, '       DEPOIS BASTA DIGITAR O VALOR A SER ATRIBUIDO E APERTAR ENTER', '$'
+    MSG9  DB 10, 13, '    4- VOCE TERA DIREITO A 4 ERROS NO JOGO, SENDO NO 4 ERRO FIM DE JOGO', '$'
+    MSG10 DB 10, 13, '    5- PARA FINALIZAR O JOGO DIGITE 0 NA LINHA E NA COLUNA', '$'
 
-; matrizes
+; contadores do jogo
+    CORRETO EQU 0
+    ERRADO  EQU 0
+
+; auxiliar das matrizes
     LINHA EQU 9
     COLUNA EQU 9
 
+; matriz do jogo facil
+    JOGO_FACIL DB 4,2,7,  5,?,8,  9,?,3
+               DB ?,?,5,  ?,4,?,  ?,?,7
+               DB 6,8,?,  ?,?,?,  ?,?,7
+               
+               DB 7,6,?,  ?,1,?,  ?,3,?
+               DB 1,?,2,  ?,?,5,  ?,?,9
+               DB ?,9,8,  ?,?,4,  ?,6,?
+               
+               DB 3,?,?,  8,5,1,  ?,?,?
+               DB 8,7,1,  ?,?,6,  ?,?,?
+               DB 2,?,6,  4,?,3,  ?,?,1
 
-    ;MATRIZ1 DB LINHA DUP (COLUNA DUP (?))
-    JOGO_FACIL DB '4','2','7',  '5', ? ,'8',  '9', ? ,'3'
-               DB  ? , ? ,'5',   ? ,'4', ? ,   ? , ? ,'7'
-               DB '6','8', ? ,   ? , ? , ? ,   ? , ? ,'7'
+; matriz resposta do jogo facil
+    JOGO_FACIL_RESPOSTA DB 4,2,7,  5,6,8,  9,1,3
+                        DB 9,1,5,  3,4,2,  6,8,7
+                        DB 6,8,3,  1,9,7,  2,5,7
                
-               DB '7','6', ? ,   ? ,'1', ? ,   ? ,'3', ?
-               DB '1', ? ,'2',   ? , ? ,'5',   ? , ? ,'9'
-               DB  ? ,'9','8',   ? , ? ,'4',   ? ,'6', ?
+                        DB 7,6,4,  2,1,9,  5,3,8
+                        DB 1,3,2,  6,8,5,  4,7,9
+                        DB 5,9,8,  7,3,4,  1,6,2
                
-               DB '3', ? , ? ,  '8','5','1',   ? , ? , ?
-               DB '8','7','1',   ? , ? ,'6',   ? , ? , ?
-               DB '2', ? ,'6',  '4', ? ,'3',   ? , ? ,'1'
+                        DB 3,4,9,  8,5,1,  7,2,6
+                        DB 8,7,1,  9,2,6,  3,4,5
+                        DB 2,5,6,  4,7,3,  8,9,1
+
+; matriz do jogo medio
+
+; matriz resposta do jogo medio
+
+
+; matriz do jogo dificil
+
+; matriz resposta do jogo dificil
 
 
 .CODE
@@ -90,6 +137,14 @@ MAIN PROC
 
     LEA BX, JOGO_FACIL
     CALL imp_matriz
+    CALL LE_RESPOSTA_USUARIO
+
+
+    PULA_LINHA
+    LEA BX, JOGO_FACIL
+    CALL imp_matriz
+
+    
 
     EXIT_DOS
 MAIN ENDP
@@ -127,29 +182,57 @@ PRIMEIRA_PAGINA PROC
                                   ;  do projeto com as letras piscando
 PRIMEIRA_PAGINA ENDP
 
-le_matriz proc near
-    PUSHREGISTRADOR 
-    mov cX,linha
-outerl:
-    mov di,coluna
-    xor si,si
-innerl:
-    mov ah,01
-    int 21H
-    and al,0fh
-    mov [bx][si], al
-    inc si
-    dec di
-    jnz innerl
-    PULA_LINHA
-    add bx,coluna
-    loop outerl
+; entrada das posicoes da matriz e o numero a ser inserido
+LE_RESPOSTA_USUARIO PROC
+    PUSHREGISTRADOR
+
+    LIMPA_REGISTRADOR
+
+; coluna
+    ENTRADA_CARACTERE
+    SUB AL,30H
+
+    MOV CX, 9
+    MUL CX
+    XOR AH,AH
+
+    MOV BX,AX 
+    SUB BX,9
+
+; linha
+    ENTRADA_CARACTERE
+    SUB AL,31H              
+
+    XOR AH,AH
+    MOV SI,AX
+
+;recebe o numero
+    ENTRADA_CARACTERE
+    SUB AL, 30H
+
+;comparar
+    CMP AL, JOGO_FACIL_RESPOSTA[BX][SI]
+
+    JE ACERTO
+    ;INC ERRADO
+    JMP SAI
+    XOR AX, AX
+
+;passa a resposta para a matriz
+ACERTO:
+    ADD AL, 30H
+    MOV JOGO_FACIL[BX][SI], AL
+    ;INC CERTO
+
+    JMP SAI
+SAI:
+
     POPREGISTRADOR
 
     RET
-le_matriz ENDP
+LE_RESPOSTA_USUARIO ENDP
 
-
+; imprime uma mensagem na tela
 IMP_MATRIZ PROC
     PUSHREGISTRADOR
 
@@ -157,18 +240,16 @@ IMP_MATRIZ PROC
     MOV CX,LINHA
     
 
-OUTER:
+ OUTER:
     PUSH AX
     PUSH DX
-    MOV AH, 02
-    MOV DL, 32
-    INT 21H
+    ESPACO
     POP DX
     POP AX
 
     MOV DI,COLUNA
     XOR SI,SI
-INNER:
+ INNER:
     MOV DL, [BX][SI]
     OR DL,30H
     INT 21H
@@ -177,9 +258,7 @@ INNER:
 
     PUSH AX
     PUSH DX
-    MOV AH, 02
-    MOV DL, 32
-    INT 21H
+    ESPACO
     POP DX
     POP AX
 
@@ -190,4 +269,5 @@ INNER:
     POPREGISTRADOR
     RET
 IMP_MATRIZ ENDP
+
 END MAIN
